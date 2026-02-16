436bd91fe26f85c7732a29822674f4d6ee5d7623
elysiasrahma <elysiasrahma@gmail.com>
2026-02-16 23:25:12 +0700

    fix: integrations for frontend in BookingsController and program.cs

--- Program.cs
+++ Program.cs
@@ -28,13 +28,6 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
 
 app.UseCors("AllowAll");
